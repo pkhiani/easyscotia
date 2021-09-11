534e3808ea8b74244bbe7d9240570ec79f7babdf
Pavan Khiani <pavan.khiani@gmail.com>
2021-09-10 20:00:22 -0700

    webscrape scotia FAQ

---
+++ webscrape.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.scotiabank.com/ca/en/commercial-banking/scotia-connect-simple/'
+
+listoflinks = ["payments.html", "administration.html",
+               "reporting.html", "additional-services.html"]
+
+for item in listoflinks:
+    res = requests.get(url + item, headers={'User-Agent': 'Mozilla/5.0'})
+
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    articles = soup.find_all("div", {"class": "card-content"})
+
+    for card in articles:
+        rows = card.find_all('div')
+
+        for row in rows:
+
+            details = row.find('p').getText().strip()
+            link = row.find("a")['href']
+
+            print(details)
+            print(link)
+
