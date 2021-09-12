eadd472886490ca0f8aa29fa6a45826df099b812
Pavan Khiani <pavan.khiani@gmail.com>
2021-09-11 17:09:15 -0700

    cotent edits

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,7 @@
- const headers = document.getElementsByTagName('p');
- for (let i = 0; i < headers.length; i++) {
-    headers[i].innerHTML = '<span style="background-color:yellow;">' + headers[i].innerHTML + '</span>';
-}
+
+// const headers = document.getElementsByTagName('p');
+//  for (let i = 0; i < headers.length; i++) {
+//     headers[i].innerHTML = '<span style="background-color:yellow;">' + headers[i].innerHTML + '</span>';
+// }
 
 
